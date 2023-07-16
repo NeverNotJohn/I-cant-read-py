cb23daaa3351c382e65be8388516dbbadbc4288a
NeverNotJohn <johntheherrera@gmail.com>
2023-07-15 20:18:39 -0400

    ugh work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Import the required module for text 
 # to speech conversion
 from gtts import gTTS
+import pydub as dub
+from pydub.playback import play
 import vlc
 import time
 
@@ -9,7 +11,7 @@ import time
 import os
   
 # The text that you want to convert to audio
-mytext = 'Welcome to geeksforgeeks!'
+mytext = 'Hello world!'
   
 # Language in which you want to convert
 language = 'en'
@@ -22,10 +24,9 @@ myobj = gTTS(text=mytext, lang=language, slow=False)
   
 # Saving the converted audio in a mp3 file named
 # welcome 
-myobj.save("audio/welcome.mp3")
+myobj.save("audio/f.mp3")
   
 # Playing the converted file
-p = vlc.MediaPlayer("audio/welcome.mp3")
 
-p.play()
-time.sleep(10)
+file = dub.AudioSegment.from_mp3("audio/f.mp3")
+play(file)
